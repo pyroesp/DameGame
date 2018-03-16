cab5449632ff622a46ea968d4b9a7d4f6a432deb
pyroesp <nicolaslopezesp@gmail.com>
2018-03-16 16:35:46 +0100

    use an array of pointers to the register to execute instructions more easily, see extended opcodes

--- cpu.c
+++ cpu.c
@@ -8,6 +8,14 @@ Cpu* cpu_Init(uint8_t *mem){
 		return NULL;
 	cpu_Reset(pCpu);
 	cpu_Set_Special_Registers(pCpu, mem);
+	pCpu->reg[0] = &pCpu->B;
+	pCpu->reg[1] = &pCpu->C;
+	pCpu->reg[2] = &pCpu->D;
+	pCpu->reg[3] = &pCpu->E;
+	pCpu->reg[4] = &pCpu->H;
+	pCpu->reg[5] = &pCpu->L;
+	pCpu->reg[6] = &pCpu->A;
+	pCpu->reg[7] = &pCpu->F;
 	return pCpu;
 }
 
@@ -45,7 +53,7 @@ void cpu_Execute_Opcode(Cpu *pCpu, uint8_t *mem){
 	if (page0[opcode].size == 2)
 		arg_byte = mem[pCpu->PC + 1];
 	else if (page0[opcode].size == 3)
-		// Check endianess
+		// Check endianness
 		arg_word = mem[pCpu->PC + 1] << 8 | mem[pCpu->PC + 2];
 
 	printf("$%04X> %02X %s\t\t%s\n", pCpu->PC, opcode, page0[opcode].mnemonic, page0[opcode].description);
@@ -77,42 +85,18 @@ void cpu_Execute_Opcode(Cpu *pCpu, uint8_t *mem){
 				pCpu->FLAG_bits.H = 0;
 				pCpu->FLAG_bits.C = 0;
 
-#define SWAP(x) x = (((x & 0xF0) >> 4) | ((x & 0x0F) << 4))
+#define SWAP(x) (x) = ((((x) & 0xF0) >> 4) | (((x) & 0x0F) << 4))
 
 				r = opcode & 0x07;
-				switch (r){
-					case 0x0: // reg B
-						SWAP(pCpu->B);
-						pCpu->FLAG_bits.Z = !(pCpu->B);
-						break;
-					case 0x1: // reg C
-						SWAP(pCpu->C);
-						pCpu->FLAG_bits.Z = !(pCpu->C);
-						break;
-					case 0x2: // reg D
-						SWAP(pCpu->D);
-						pCpu->FLAG_bits.Z = !(pCpu->D);
-						break;
-					case 0x3: // reg E
-						SWAP(pCpu->E);
-						pCpu->FLAG_bits.Z = !(pCpu->E);
-						break;
-					case 0x4: // reg H
-						SWAP(pCpu->H);
-						pCpu->FLAG_bits.Z = !(pCpu->H);
-						break;
-					case 0x5: // reg L
-						SWAP(pCpu->L);
-						pCpu->FLAG_bits.Z = !(pCpu->L);
-						break;
-					case 0x6: // reg (HL)
-						SWAP(mem[pCpu->HL]);
-						pCpu->FLAG_bits.Z = !(mem[pCpu->HL]);
-						break;
-					case 0x7: // reg A
-						SWAP(pCpu->A);
-						pCpu->FLAG_bits.Z = !(pCpu->A);
-						break;
+				if (r < 0x06){
+					SWAP(*(pCpu->reg[r]));
+					pCpu->FLAG_bits.Z = !(*(pCpu->reg[r]));
+				} else if (r == 0x06){
+					SWAP(mem[pCpu->HL]);
+					pCpu->FLAG_bits.Z = !(mem[pCpu->HL]);
+				} else if (r == 0x07){
+					SWAP(pCpu->A);
+					pCpu->FLAG_bits.Z = !(pCpu->A);
 				}
 				break;
 			case 0x38: // SRL
@@ -120,47 +104,18 @@ void cpu_Execute_Opcode(Cpu *pCpu, uint8_t *mem){
 				pCpu->FLAG_bits.H = 0;
 
 				r = opcode & 0x07;
-				switch (r){
-					case 0x0: // reg B
-						pCpu->FLAG_bits.C = pCpu->B & 0x01;
-						pCpu->B = 0x7F & (pCpu->B >> 1);
-						pCpu->FLAG_bits.Z = pCpu->B == 0;
-						break;
-					case 0x1: // reg C
-						pCpu->FLAG_bits.C = pCpu->C & 0x01;
-						pCpu->C = 0x7F & (pCpu->C >> 1);
-						pCpu->FLAG_bits.Z = pCpu->C == 0;
-						break;
-					case 0x2: // reg D
-						pCpu->FLAG_bits.C = pCpu->D & 0x01;
-						pCpu->D = 0x7F & (pCpu->D >> 1);
-						pCpu->FLAG_bits.Z = pCpu->D == 0;
-						break;
-					case 0x3: // reg E
-						pCpu->FLAG_bits.C = pCpu->E & 0x01;
-						pCpu->E = 0x7F & (pCpu->E >> 1);
-						pCpu->FLAG_bits.Z = pCpu->E == 0;
-						break;
-					case 0x4: // reg H
-						pCpu->FLAG_bits.C = pCpu->H & 0x01;
-						pCpu->H = 0x7F & (pCpu->H >> 1);
-						pCpu->FLAG_bits.Z = pCpu->H == 0;
-						break;
-					case 0x5: // reg L
-						pCpu->FLAG_bits.C = pCpu->L & 0x01;
-						pCpu->L = 0x7F & (pCpu->L >> 1);
-						pCpu->FLAG_bits.Z = pCpu->L == 0;
-						break;
-					case 0x6: // reg (HL)
-						pCpu->FLAG_bits.C = mem[pCpu->HL] & 0x01;
-						mem[pCpu->HL] = 0x7F & (mem[pCpu->HL] >> 1);
-						pCpu->FLAG_bits.Z = mem[pCpu->HL] == 0;
-						break;
-					case 0x7: // reg A
-						pCpu->FLAG_bits.C = pCpu->A & 0x01;
-						pCpu->A = 0x7F & (pCpu->A >> 1);
-						pCpu->FLAG_bits.Z = pCpu->A == 0;
-						break;
+				if (r < 0x06){
+					pCpu->FLAG_bits.C = *(pCpu->reg[r]) & 0x01;
+					pCpu->B = 0x7F & (*(pCpu->reg[r]) >> 1);
+					pCpu->FLAG_bits.Z = *(pCpu->reg[r]) == 0;
+				} else if (r == 0x06){
+					pCpu->FLAG_bits.C = mem[pCpu->HL] & 0x01;
+					mem[pCpu->HL] = 0x7F & (mem[pCpu->HL] >> 1);
+					pCpu->FLAG_bits.Z = mem[pCpu->HL] == 0;
+				} else if (r == 0x07){
+					pCpu->FLAG_bits.C = pCpu->A & 0x01;
+					pCpu->A = 0x7F & (pCpu->A >> 1);
+					pCpu->FLAG_bits.Z = pCpu->A == 0;
 				}
 				break;
 			case 0x40: // BIT 0
@@ -179,32 +134,12 @@ void cpu_Execute_Opcode(Cpu *pCpu, uint8_t *mem){
 				pCpu->FLAG_bits.H = 1;
 
 				r = opcode & 0x07;
-				switch (r){
-					case 0x0: // reg B
-						pCpu->FLAG_bits.Z = !(pCpu->B & mask);
-						break;
-					case 0x1: // reg C
-						pCpu->FLAG_bits.Z = !(pCpu->C & mask);
-						break;
-					case 0x2: // reg D
-						pCpu->FLAG_bits.Z = !(pCpu->D & mask);
-						break;
-					case 0x3: // reg E
-						pCpu->FLAG_bits.Z = !(pCpu->E & mask);
-						break;
-					case 0x4: // reg H
-						pCpu->FLAG_bits.Z = !(pCpu->H & mask);
-						break;
-					case 0x5: // reg L
-						pCpu->FLAG_bits.Z = !(pCpu->L & mask);
-						break;
-					case 0x6: // reg (HL)
-						pCpu->FLAG_bits.Z = !(mem[pCpu->HL] & mask);
-						break;
-					case 0x7: // reg A
-						pCpu->FLAG_bits.Z = !(pCpu->A & mask);
-						break;
-				}
+				if (r < 0x06)
+					pCpu->FLAG_bits.Z = !(*(pCpu->reg[r]) & mask);
+				else if (r == 0x06)
+					pCpu->FLAG_bits.Z = !(mem[pCpu->HL] & mask);
+				else if (r == 0x07)
+					pCpu->FLAG_bits.Z = !(pCpu->A & mask);
 				break;
 			case 0x80: // RES 0
 			case 0x88: // RES 1
@@ -219,32 +154,12 @@ void cpu_Execute_Opcode(Cpu *pCpu, uint8_t *mem){
 				mask = 1 << bit;
 
 				r = opcode & 0x07;
-				switch (r){
-					case 0x0: // reg B
-						pCpu->B &= ~mask;
-						break;
-					case 0x1: // reg C
-						pCpu->C &= ~mask;
-						break;
-					case 0x2: // reg D
-						pCpu->D &= ~mask;
-						break;
-					case 0x3: // reg E
-						pCpu->E &= ~mask;
-						break;
-					case 0x4: // reg H
-						pCpu->H &= ~mask;
-						break;
-					case 0x5: // reg L
-						pCpu->L &= ~mask;
-						break;
-					case 0x6: // reg (HL)
-						mem[pCpu->HL] &= ~mask;
-						break;
-					case 0x7: // reg A
-						pCpu->A &= ~mask;
-						break;
-				}
+				if (r < 0x06)
+					*(pCpu->reg[r]) &= ~mask;
+				else if (r == 0x06)
+					mem[pCpu->HL] &= ~mask;
+				else if (r == 0x07)
+					pCpu->A &= ~mask;
 				break;
 			case 0xC0: // SET 0
 			case 0xC8: // SET 1
@@ -259,50 +174,15 @@ void cpu_Execute_Opcode(Cpu *pCpu, uint8_t *mem){
 				mask = 1 << bit;
 
 				r = opcode & 0x07;
-
-				/*
-				TODO : Check if Cpu->reg is correctly allign so that
-						Cpu->reg[0] is the same as Cpu->B
-						Cpu->reg[1] is the same as Cpu->C
-						...
-						If so, then change all switch (r){} to if statement below
-
-						if (r < 0x06)
-							pCpu->reg[r] |= mask;
-						else if (r == 0x06)
-							mem[pCpu->HL] |= mask;
-						else if (r == 0x07)
-							pCpu->A |= mask;
-				*/
-
-				switch (r){
-					case 0x0: // reg B
-						pCpu->B |= mask;
-						break;
-					case 0x1: // reg C
-						pCpu->C |= mask;
-						break;
-					case 0x2: // reg D
-						pCpu->D |= mask;
-						break;
-					case 0x3: // reg E
-						pCpu->E |= mask;
-						break;
-					case 0x4: // reg H
-						pCpu->H |= mask;
-						break;
-					case 0x5: // reg L
-						pCpu->L |= mask;
-						break;
-					case 0x6: // reg (HL)
-						mem[pCpu->HL] |= mask;
-						break;
-					case 0x7: // reg A
-						pCpu->A |= mask;
-						break;
-				}
+				if (r < 0x06)
+					*(pCpu->reg[r]) |= mask;
+				else if (r == 0x06)
+					mem[pCpu->HL] |= mask;
+				else if (r == 0x07)
+					pCpu->A |= mask;
 				break;
 			default:
+				printf("unknown instruction CB%02X\n", opcode);
 				break;
 		}
 	}else{ // page0 opcodes
--- cpu.h
+++ cpu.h
@@ -16,58 +16,53 @@
 
 typedef struct{
 	uint64_t clock_cycle; // 4 x machine cycle
-
-	union{
-		// TODO: Check if correctly alligned so that
-		// 0 to 5 -> B, C, D, E, H, L
-		uint8_t reg[8]; // Easier for executing opcodes
-		union{
-			uint16_t BC;
-			struct{
-				uint8_t C;
-				uint8_t B;
-			};
-		};
-		union{
-			uint16_t DE;
-			struct{
-				uint8_t E;
-				uint8_t D;
-			};
-		};
-		union{
-			uint16_t HL;
-			struct{
-				uint8_t L;
-				uint8_t H;
-			};
-		};
-		union{
-			uint16_t AF;
-			struct{
-				union{
-					uint8_t F;
-					uint8_t FLAG;
-					struct{
-						uint8_t unused : 4;
-						uint8_t C : 1;
-						uint8_t H : 1;
-						uint8_t N : 1;
-						uint8_t Z : 1;
-					}FLAG_bits;
-					uint8_t STATUS;
-					struct{
-						uint8_t unused : 4;
-						uint8_t carry : 1;
-						uint8_t half_carry : 1;
-						uint8_t substract : 1;
-						uint8_t zero : 1;
-					}STATUS_flags;
-				};
-				uint8_t A;
-			};
-		};
+    union{
+        uint16_t BC;
+        struct{
+            uint8_t C;
+            uint8_t B;
+        };
+    };
+    union{
+        uint16_t DE;
+        struct{
+            uint8_t E;
+            uint8_t D;
+        };
+    };
+    union{
+        uint16_t HL;
+        struct{
+            uint8_t L;
+            uint8_t H;
+        };
+    };
+    union{
+        uint16_t AF;
+        struct{
+            union{
+                uint8_t F;
+                uint8_t FLAG;
+                struct{
+                    uint8_t unused : 4;
+                    uint8_t C : 1;
+                    uint8_t H : 1;
+                    uint8_t N : 1;
+                    uint8_t Z : 1;
+                }FLAG_bits;
+                uint8_t STATUS;
+                struct{
+                    uint8_t unused : 4;
+                    uint8_t carry : 1;
+                    uint8_t half_carry : 1;
+                    uint8_t substract : 1;
+                    uint8_t zero : 1;
+                }STATUS_flags;
+            };
+            uint8_t A;
+        };
 	};
+	uint8_t *reg[8];
 	uint16_t SP; // decrements before putting something on the stack
 	uint16_t PC;
 	union Special_Register *sfr;
