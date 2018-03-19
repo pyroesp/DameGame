bed9e98ca56e36e6e5601f107fcae6e8e488fdf0
pyroesp <nicolaslopezesp@gmail.com>
2018-03-19 21:13:39 +0100

    added REG_X defines, note reg_A is 7 and not 6, makes it easier for executing instructions

--- cpu.h
+++ cpu.h
@@ -14,6 +14,15 @@
 
 */
 
+#define REG_B 0
+#define REG_C 1
+#define REG_D 2
+#define REG_E 3
+#define REG_H 4
+#define REG_L 5
+#define REG_A 7
+#define REG_F 6
+
 union Cpu_Register{
 	uint8_t R;
 	struct{
