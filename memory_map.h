0d10ca295017b4d19e0155f8500972095a54150c
pyroesp <nicolaslopezesp@gmail.com>
2018-03-21 07:01:49 +0100

    minor update

--- memory_map.h
+++ memory_map.h
@@ -1,6 +1,8 @@
 #ifndef _MEMORY_MAP_H
 #define _MEMORY_MAP_H
 
+#include "rom.h"
+#include "ram.h"
 
 /*
 	Interrupt Enable Register
@@ -29,27 +31,23 @@
 	--------------------------- 0000
 */
 
-#define MEM_ROM_0_SIZE (0x4000)
-#define MEM_ROM_SWITCH_SIZE (0x4000)
-#define MEM_RAM_VIDEO_SIZE (0x2000)
-#define MEM_RAM_SWITCH_SIZE (0x2000)
-#define MEM_RAM_INTERNAL_SIZE (0x2000)
-#define MEM_RAM_INTERNAL_ECHO_SIZE (0x2000)
+#define MEM_TOTAL_SIZE (0x10000)
+
+#define MEM_RAM_INTERNAL_ECHO_SIZE (0x1E00)
 #define MEM_SPRITE_ATTRI_SIZE (0x00A0)
-#define MEM_IO_PORTS_SIZE (0x004C)
-#define MEM_RAM_INTERNAL2_SIZE (0x0080)
+#define MEM_UNUSABLE_SIZE (0x0060)
+#define MEM_IO_PORTS_SIZE (0x00FF)
 
-#define MEM_TOTAL (0xFFFF)
-
-
-#define MEM_ROM_0_OFFSET (0x0000)
-#define MEM_ROM_SWITCH_OFFSET (0x4000)
+#define MEM_ROM_BANK0_OFFSET (0x0000)
+#define MEM_ROM_SWITCH_BANK_OFFSET (0x4000)
 #define MEM_RAM_VIDEO_OFFSET (0x8000)
 #define MEM_RAM_SWITCH_OFFSET (0xA000)
 #define MEM_RAM_INTERNAL_OFFSET (0xC000)
 #define MEM_RAM_INTERNAL_ECHO_OFFSET (0xE000)
 #define MEM_SPRITE_ATTRI_OFFSET (0xFE00)
+#define MEM_UNUSABLE_OFFSET (0xFEA0)
 #define MEM_IO_PORTS_OFFSET (0xFF00)
 #define MEM_RAM_INTERNAL2_OFFSET (0xFF80)
 
 #endif
+
