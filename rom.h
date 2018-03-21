fbeefd00cb932f0f1bf3e31d32ab8b02f96fd927
pyroesp <nicolaslopezesp@gmail.com>
2018-03-21 07:02:35 +0100

    added rom.h and ram.h

---
+++ rom.h
@@ -0,0 +1,44 @@
+#ifndef _ROM_H
+#define _ROM_H
+
+#include <stdint.h>
+
+#define ROM_SIZE (0x40000)
+#define ROM_BANK_SIZE (0x4000)
+
+#define ROM_BANK_0 (0)
+#define ROM_BANK_1 (1)
+#define ROM_BANK_2 (2)
+#define ROM_BANK_3 (3)
+#define ROM_BANK_4 (4)
+#define ROM_BANK_5 (5)
+#define ROM_BANK_6 (6)
+#define ROM_BANK_7 (7)
+#define ROM_BANK_8 (8)
+#define ROM_BANK_9 (9)
+#define ROM_BANK_10 (10)
+#define ROM_BANK_11 (11)
+#define ROM_BANK_12 (12)
+#define ROM_BANK_13 (13)
+#define ROM_BANK_14 (14)
+#define ROM_BANK_15 (15)
+#define ROM_TOTAL_BANKS (16)
+
+#define ROM_BANK_0_OFFSET (0x0000)
+#define ROM_BANK_1_OFFSET (0x4000)
+#define ROM_BANK_2_OFFSET (0x8000)
+#define ROM_BANK_3_OFFSET (0xC000)
+#define ROM_BANK_4_OFFSET (0x10000)
+#define ROM_BANK_5_OFFSET (0x14000)
+#define ROM_BANK_6_OFFSET (0x18000)
+#define ROM_BANK_7_OFFSET (0x1C000)
+#define ROM_BANK_8_OFFSET (0x20000)
+#define ROM_BANK_9_OFFSET (0x24000)
+#define ROM_BANK_10_OFFSET (0x28000)
+#define ROM_BANK_11_OFFSET (0x2C000)
+#define ROM_BANK_12_OFFSET (0x30000)
+#define ROM_BANK_13_OFFSET (0x34000)
+#define ROM_BANK_14_OFFSET (0x38000)
+#define ROM_BANK_15_OFFSET (0x3C000)
+
+#endif
