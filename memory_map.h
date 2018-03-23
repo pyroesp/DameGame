81defa94b3d8263e8259d282452c1e2d0763d5d9
pyroesp <nicolaslopezesp@gmail.com>
2018-03-23 06:27:35 +0100

    added memory map structure, not sure yet if this is the way to go

--- memory_map.h
+++ memory_map.h
@@ -1,6 +1,8 @@
 #ifndef _MEMORY_MAP_H
 #define _MEMORY_MAP_H
 
+#include <stdlib.h>
+#include <stdint.h>
 #include "rom.h"
 #include "ram.h"
 
@@ -33,10 +35,12 @@
 
 #define MEM_TOTAL_SIZE (0x10000)
 
+#define MEM_VIDEO_RAM_SIZE (0x2000)
+#define MEM_RAM_INTERNAL_SIZE (0x4000) // internal_ram -> [C000; FFFF]
 #define MEM_RAM_INTERNAL_ECHO_SIZE (0x1E00)
-#define MEM_SPRITE_ATTRI_SIZE (0x00A0)
+#define MEM_SPRITE_ATTRI_SIZE (0x00A1)
 #define MEM_UNUSABLE_SIZE (0x0060)
-#define MEM_IO_PORTS_SIZE (0x00FF)
+#define MEM_IO_PORTS_SIZE (0x0100)
 
 #define MEM_ROM_BANK0_OFFSET (0x0000)
 #define MEM_ROM_SWITCH_BANK_OFFSET (0x4000)
@@ -48,6 +52,29 @@
 #define MEM_UNUSABLE_OFFSET (0xFEA0)
 #define MEM_IO_PORTS_OFFSET (0xFF00)
 #define MEM_RAM_INTERNAL2_OFFSET (0xFF80)
+
+
+typedef struct{
+	// ROM bank 0
+	uint8_t *rom_bank_0; // 0x0000 0x4000
+	// ROM bank switchable
+	uint8_t *rom_bank_switch; // 0x4000 0x4000
+	// Video RAM
+	uint8_t *video_ram; // 0x8000 0x2000
+	// RAM bank switchable
+	uint8_t *ram_bank_switch; // 0xA000 0x2000
+	// RAM internal
+	uint8_t *ram_int; // 0xC000 0x4000
+	// RAM internal echo 
+	uint8_t *ram_bank_int_echo; // 0xE000 0x1E00
+	// Object Attribute memory aka sprite
+	uint8_t *oam; // 0xFE00 0x00A0
+	// Unusable mem
+	uint8_t *unusable_mem; // 0xFEA0 0x0060
+	// IO ports
+	uint8_t *special_register; // 0xFF00 0x00FF
+}MemoryMap;
+
 
 #endif
 
