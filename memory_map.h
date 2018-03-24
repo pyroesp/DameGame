28124236ef828e2cf746fd2d04375f95cdcef1a3
pyroesp <nicolaslopezesp@gmail.com>
2018-03-24 12:38:18 +0100

    removed big structure, added memory.h include, added BIOS size, added smaller/easier to use structure, added enum list of things in the CPU space address, ...

--- memory_map.h
+++ memory_map.h
@@ -3,46 +3,37 @@
 
 #include <stdlib.h>
 #include <stdint.h>
+#include "memory.h"
 #include "rom.h"
 #include "ram.h"
 
-/*
-	Interrupt Enable Register
-	--------------------------- FFFF
-	Internal RAM
-	--------------------------- FF80
-	Empty but unusable for I/O
-	--------------------------- FF4C
-	I/O ports
-	--------------------------- FF00
-	Empty but unusable for I/O
-	--------------------------- FEA0
-	Sprite Attrib Memory (OAM)
-	--------------------------- FE00
-	Echo of 8kB Internal RAM
-	--------------------------- E000
-	8kB Internal RAM
-	--------------------------- C000
-	8kB switchable RAM bank
-	--------------------------- A000
-	8kB Video RAM
-	--------------------------- 8000
-	16kB switchable ROM bank
-	--------------------------- 4000
-	16kB ROM bank #0
-	--------------------------- 0000
-*/
-
 #define MEM_TOTAL_SIZE (0x10000)
 
+#define MEM_ADDRESS_SPACES (10)
+
+enum{
+	MAP_ROM_BIOS, // 0000 - 00FF
+	MAP_ROM_BANK_0, // 0000 - 3FFF
+	MAP_ROM_BANK_SWITCH, // 4000 - 7FFF
+	MAP_VRAM, // 8000 - 9FFF
+	MAP_RAM_BANK_SWITCH, // A000 - BFFF
+	MAP_RAM_INTERNAL, // C000 - DFFF
+	MAP_RAM_INTERNAL_ECHO, // E000 - FDFF
+	MAP_OAM, // FE00 - FE9F
+	MAP_UNUSABLE, // FEA0 - FE9F
+	MAP_IO_PORTS, // FF00 - FFFF
+};
+
+#define MEM_ROM_BIOS_SIZE (0x100)
 #define MEM_VIDEO_RAM_SIZE (0x2000)
 #define MEM_RAM_INTERNAL_SIZE (0x4000) // internal_ram -> [C000; FFFF]
 #define MEM_RAM_INTERNAL_ECHO_SIZE (0x1E00)
-#define MEM_SPRITE_ATTRI_SIZE (0x00A1)
+#define MEM_SPRITE_ATTRI_SIZE (0x00A0) // OAM
 #define MEM_UNUSABLE_SIZE (0x0060)
 #define MEM_IO_PORTS_SIZE (0x0100)
 
-#define MEM_ROM_BANK0_OFFSET (0x0000)
+#define MEM_ROM_BIOS_OFFSET (0x0000)
+#define MEM_ROM_BANK_0_OFFSET (0x0000)
 #define MEM_ROM_SWITCH_BANK_OFFSET (0x4000)
 #define MEM_RAM_VIDEO_OFFSET (0x8000)
 #define MEM_RAM_SWITCH_OFFSET (0xA000)
@@ -55,24 +46,8 @@
 
 
 typedef struct{
-	// ROM bank 0
-	uint8_t *rom_bank_0; // 0x0000 0x4000
-	// ROM bank switchable
-	uint8_t *rom_bank_switch; // 0x4000 0x4000
-	// Video RAM
-	uint8_t *video_ram; // 0x8000 0x2000
-	// RAM bank switchable
-	uint8_t *ram_bank_switch; // 0xA000 0x2000
-	// RAM internal
-	uint8_t *ram_int; // 0xC000 0x4000
-	// RAM internal echo 
-	uint8_t *ram_bank_int_echo; // 0xE000 0x1E00
-	// Object Attribute memory aka sprite
-	uint8_t *oam; // 0xFE00 0x00A0
-	// Unusable mem
-	uint8_t *unusable_mem; // 0xFEA0 0x0060
-	// IO ports
-	uint8_t *special_register; // 0xFF00 0x00FF
+	Memory mem; // RAM, ROM, VRAM, ...
+	uint16_t map_offset; // address offset of memory map
 }MemoryMap;
 
 
