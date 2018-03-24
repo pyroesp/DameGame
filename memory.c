6a262377b656a47bce4fd40757bcad195d52d1e1
pyroesp <nicolaslopezesp@gmail.com>
2018-03-24 12:39:44 +0100

    added 2 fonctions, not sure if I'm going to keep these yet

--- memory.c
+++ memory.c
@@ -8,7 +8,7 @@ Memory *mem_Init(uint32_t size, uint32_t banks, uint32_t bank_size){
 		mem->size = size;
 		mem->banks = banks;
 		mem->bank_size = bank_size;
-
+		mem->start_idx = 0;
 		mem->data = (uint8_t*)malloc(sizeof(uint8_t) * size);
 
 		return mem;
@@ -24,32 +24,42 @@ void mem_Free(Memory *pMem){
 	pMem = NULL;
 }
 
+void mem_SetStartIndex(Memory *pMem, uint32_t start_idx){
+	pMem->start_idx = start_idx;
+}
+
+void mem_CopyInfo(Memory *pDest, Memory *pSrc){
+	pDest->data = pSrc->data;
+	pDest->banks = pSrc->banks;
+	pDest->bank_size = pSrc->bank_size;
+	pDest->start_idx = pSrc->start_idx;
+}
 
 uint16_t mem_Read(Memory *pMem, uint32_t address){
-	if (address < pMem->size)
-		return pMem->data[address];
+	if (pMem->start_idx + address < pMem->size)
+		return pMem->data[pMem->start_idx + address];
 	return 0x100; // error for now is 256
 }
 
 uint8_t mem_ReadMulti(Memory *pMem, uint32_t address, uint8_t *data, uint32_t read_size){
-	if (address + read_size < pMem->size){
-		memcpy(data, &pMem->data[address], sizeof(uint8_t) * read_size);
+	if (pMem->start_idx + address + read_size < pMem->size){
+		memcpy(data, &pMem->data[pMem->start_idx + address], sizeof(uint8_t) * read_size);
 		return 1;
 	}
 	return 0;
 }
 
 uint16_t mem_Write(Memory *pMem, uint32_t address, uint8_t data){
-	if (address < pMem->size){
-		pMem->data[address] = data;
+	if (pMem->start_idx + address < pMem->size){
+		pMem->data[pMem->start_idx + address] = data;
 		return data;
 	}
 	return 0x100; // error for now is 256
 }
 
 uint8_t mem_WriteMulti(Memory *pMem, uint32_t address, uint8_t *data, uint32_t write_size){
-	if (address + write_size < pMem->size){
-		memcpy(&pMem->data[address], data, sizeof(uint8_t) * write_size);
+	if (pMem->start_idx + address + write_size < pMem->size){
+		memcpy(&pMem->data[pMem->start_idx + address], data, sizeof(uint8_t) * write_size);
 		return 1;
 	}
 	return 0;
--- memory.h
+++ memory.h
@@ -11,12 +11,15 @@ typedef struct{
 	uint32_t size;
 	uint32_t banks;
 	uint32_t bank_size;
+	uint32_t start_idx;
 }Memory;
 
 
 Memory *mem_Init(uint32_t size, uint32_t banks, uint32_t bank_size);
 void mem_Free(Memory *pMem);
 
+void mem_SetStartIndex(Memory *pMem, uint32_t start_idx);
+void mem_CopyInfo(Memory *pDest, Memory *pSrc);
 
 uint16_t mem_Read(Memory *pMem, uint32_t address);
 uint8_t mem_ReadMulti(Memory *pMem, uint32_t address, uint8_t *data, uint32_t read_size);
