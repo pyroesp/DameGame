a0ffa6b232aa6d69c63afd4cae230db0394d3dad
pyroesp <nicolaslopezesp@gmail.com>
2018-03-25 05:55:43 +0200

    MemoryMap is now initiliazed inside the cpu_Init function. Setting the SFR pointer is done outside the cpu_Init as it needs to point to Internal_RAM, which is not yet defined in the cpu_Init function. disable the bios via the sfr->bios register.

--- main.c
+++ main.c
@@ -7,13 +7,14 @@
 #include "cpu.h"
 
 int main(int argc, char *argv[]){
+	Cpu *cpu = NULL;
 	Memory *BIOS = NULL;
 	Memory *ROM = NULL;
 	Memory *RAM = NULL;
 	Memory *Internal_RAM = NULL;
 	Memory *Display_RAM = NULL;
 
-	MemoryMap cpu_map[MEM_ADDRESS_SPACES];
+	cpu = cpu_Init();
 
 	BIOS = mem_Init(MEM_ROM_BIOS_SIZE, 1, MEM_ROM_BIOS_SIZE);
 	ROM = mem_Init(ROM_SIZE, ROM_BANK_SIZE, ROM_SIZE / ROM_BANK_SIZE);
@@ -21,69 +22,72 @@ int main(int argc, char *argv[]){
 	Internal_RAM = mem_Init(MEM_RAM_INTERNAL_SIZE, 1, MEM_RAM_INTERNAL_SIZE);
 	Display_RAM = mem_Init(MEM_VIDEO_RAM_SIZE, 1, MEM_VIDEO_RAM_SIZE);
 
-
 	// set up BIOS
-	mem_CopyInfo(&cpu_map[MAP_ROM_BIOS].mem, BIOS);
-	cpu_map[MAP_ROM_BIOS].map_offset = MEM_ROM_BIOS_OFFSET;
+	mem_CopyInfo(&cpu->map[MAP_ROM_BIOS].mem, BIOS);
+	cpu->map[MAP_ROM_BIOS].offset = MEM_ROM_BIOS_OFFSET;
 
 	// set up ROM
-	mem_CopyInfo(&cpu_map[MAP_ROM_BANK_0].mem, ROM);
-	cpu_map[MAP_ROM_BANK_0].map_offset = MEM_ROM_BANK_0_OFFSET;
+	mem_CopyInfo(&cpu->map[MAP_ROM_BANK_0].mem, ROM);
+	cpu->map[MAP_ROM_BANK_0].offset = MEM_ROM_BANK_0_OFFSET;
 
 	// set up ROM switch bank
-	mem_CopyInfo(&cpu_map[MAP_ROM_BANK_SWITCH].mem, ROM);
-	mem_SetStartIndex(&cpu_map[MAP_ROM_BANK_SWITCH].mem, cpu_map[MAP_ROM_BANK_SWITCH].mem.bank_size);
-	cpu_map[MAP_ROM_BANK_SWITCH].map_offset = MEM_ROM_SWITCH_BANK_OFFSET;
+	mem_CopyInfo(&cpu->map[MAP_ROM_BANK_SWITCH].mem, ROM);
+	mem_SetStartIndex(&cpu->map[MAP_ROM_BANK_SWITCH].mem, cpu->map[MAP_ROM_BANK_SWITCH].mem.bank_size);
+	cpu->map[MAP_ROM_BANK_SWITCH].offset = MEM_ROM_SWITCH_BANK_OFFSET;
 
 	// set up Video RAM
-	mem_CopyInfo(&cpu_map[MAP_VRAM].mem, Display_RAM);
-	cpu_map[MAP_VRAM].map_offset = MEM_RAM_VIDEO_OFFSET;
+	mem_CopyInfo(&cpu->map[MAP_VRAM].mem, Display_RAM);
+	cpu->map[MAP_VRAM].offset = MEM_RAM_VIDEO_OFFSET;
 
 	// set up RAM switch bank
-	mem_CopyInfo(&cpu_map[MAP_RAM_BANK_SWITCH].mem, RAM);
-	cpu_map[MAP_RAM_BANK_SWITCH].map_offset = MEM_RAM_SWITCH_OFFSET;
+	mem_CopyInfo(&cpu->map[MAP_RAM_BANK_SWITCH].mem, RAM);
+	cpu->map[MAP_RAM_BANK_SWITCH].offset = MEM_RAM_SWITCH_OFFSET;
 
 	// set up internal RAM
-	cpu_map[MAP_RAM_INTERNAL].mem.data = Internal_RAM->data;
-	cpu_map[MAP_RAM_INTERNAL].mem.banks = 1;
-	cpu_map[MAP_RAM_INTERNAL].mem.size = RAM_BANK_SIZE;
-	cpu_map[MAP_RAM_INTERNAL].mem.bank_size = RAM_BANK_SIZE;
-	cpu_map[MAP_RAM_INTERNAL].mem.start_idx = 0;
-	cpu_map[MAP_RAM_INTERNAL].map_offset = MEM_RAM_INTERNAL_OFFSET;
+	cpu->map[MAP_RAM_INTERNAL].mem.data = Internal_RAM->data;
+	cpu->map[MAP_RAM_INTERNAL].mem.banks = 1;
+	cpu->map[MAP_RAM_INTERNAL].mem.size = RAM_BANK_SIZE;
+	cpu->map[MAP_RAM_INTERNAL].mem.bank_size = RAM_BANK_SIZE;
+	cpu->map[MAP_RAM_INTERNAL].mem.start_idx = 0;
+	cpu->map[MAP_RAM_INTERNAL].offset = MEM_RAM_INTERNAL_OFFSET;
 
 	// set up echo of internal RAM
-	cpu_map[MAP_RAM_INTERNAL_ECHO].mem.data = Internal_RAM->data;
-	cpu_map[MAP_RAM_INTERNAL_ECHO].mem.banks = 1;
-	cpu_map[MAP_RAM_INTERNAL_ECHO].mem.size = MEM_RAM_INTERNAL_ECHO_SIZE;
-	cpu_map[MAP_RAM_INTERNAL_ECHO].mem.bank_size = MEM_RAM_INTERNAL_ECHO_SIZE;
-	cpu_map[MAP_RAM_INTERNAL_ECHO].mem.start_idx = 0;
-	cpu_map[MAP_RAM_INTERNAL_ECHO].map_offset = MEM_RAM_INTERNAL_ECHO_OFFSET;
+	cpu->map[MAP_RAM_INTERNAL_ECHO].mem.data = Internal_RAM->data;
+	cpu->map[MAP_RAM_INTERNAL_ECHO].mem.banks = 1;
+	cpu->map[MAP_RAM_INTERNAL_ECHO].mem.size = MEM_RAM_INTERNAL_ECHO_SIZE;
+	cpu->map[MAP_RAM_INTERNAL_ECHO].mem.bank_size = MEM_RAM_INTERNAL_ECHO_SIZE;
+	cpu->map[MAP_RAM_INTERNAL_ECHO].mem.start_idx = 0;
+	cpu->map[MAP_RAM_INTERNAL_ECHO].offset = MEM_RAM_INTERNAL_ECHO_OFFSET;
 
 	// set up object attribute mem
-	cpu_map[MAP_OAM].mem.data = &Internal_RAM->data[MEM_SPRITE_ATTRI_OFFSET - MEM_RAM_INTERNAL_OFFSET];
-	cpu_map[MAP_OAM].mem.banks = 1;
-	cpu_map[MAP_OAM].mem.size = MEM_SPRITE_ATTRI_SIZE;
-	cpu_map[MAP_OAM].mem.bank_size = MEM_SPRITE_ATTRI_SIZE;
-	cpu_map[MAP_OAM].mem.start_idx = 0;
-	cpu_map[MAP_OAM].map_offset = MEM_SPRITE_ATTRI_OFFSET;
+	cpu->map[MAP_OAM].mem.data = &Internal_RAM->data[MEM_SPRITE_ATTRI_OFFSET - MEM_RAM_INTERNAL_OFFSET];
+	cpu->map[MAP_OAM].mem.banks = 1;
+	cpu->map[MAP_OAM].mem.size = MEM_SPRITE_ATTRI_SIZE;
+	cpu->map[MAP_OAM].mem.bank_size = MEM_SPRITE_ATTRI_SIZE;
+	cpu->map[MAP_OAM].mem.start_idx = 0;
+	cpu->map[MAP_OAM].offset = MEM_SPRITE_ATTRI_OFFSET;
 
 	// set up unusable section of map, not really needed
-	cpu_map[MAP_UNUSABLE].mem.data = &Internal_RAM->data[MEM_UNUSABLE_OFFSET - MEM_RAM_INTERNAL_OFFSET];
-	cpu_map[MAP_UNUSABLE].mem.banks = 1;
-	cpu_map[MAP_UNUSABLE].mem.size = MEM_UNUSABLE_SIZE;
-	cpu_map[MAP_UNUSABLE].mem.bank_size = MEM_UNUSABLE_SIZE;
-	cpu_map[MAP_UNUSABLE].mem.start_idx = 0;
-	cpu_map[MAP_UNUSABLE].map_offset = MEM_UNUSABLE_OFFSET;
+	cpu->map[MAP_HRAM].mem.data = &Internal_RAM->data[MEM_HRAM_OFFSET - MEM_RAM_INTERNAL_OFFSET];
+	cpu->map[MAP_HRAM].mem.banks = 1;
+	cpu->map[MAP_HRAM].mem.size = MEM_HRAM_SIZE;
+	cpu->map[MAP_HRAM].mem.bank_size = MEM_HRAM_SIZE;
+	cpu->map[MAP_HRAM].mem.start_idx = 0;
+	cpu->map[MAP_HRAM].offset = MEM_HRAM_OFFSET;
 
 	// set up IO ports, including interrupt enable register
-	cpu_map[MAP_IO_PORTS].mem.data = &Internal_RAM->data[MEM_IO_PORTS_OFFSET - MEM_RAM_INTERNAL_OFFSET];
-	cpu_map[MAP_IO_PORTS].mem.banks = 1;
-	cpu_map[MAP_IO_PORTS].mem.size = MEM_IO_PORTS_SIZE;
-	cpu_map[MAP_IO_PORTS].mem.bank_size = MEM_IO_PORTS_SIZE;
-	cpu_map[MAP_IO_PORTS].mem.start_idx = 0;
-	cpu_map[MAP_IO_PORTS].map_offset = MEM_IO_PORTS_OFFSET;
+	cpu->map[MAP_IO_PORTS].mem.data = &Internal_RAM->data[MEM_IO_PORTS_OFFSET - MEM_RAM_INTERNAL_OFFSET];
+	cpu->map[MAP_IO_PORTS].mem.banks = 1;
+	cpu->map[MAP_IO_PORTS].mem.size = MEM_IO_PORTS_SIZE;
+	cpu->map[MAP_IO_PORTS].mem.bank_size = MEM_IO_PORTS_SIZE;
+	cpu->map[MAP_IO_PORTS].mem.start_idx = 0;
+	cpu->map[MAP_IO_PORTS].offset = MEM_IO_PORTS_OFFSET;
 
+    // Set SFR pointer
+	cpu_SetSpecialRegisters(cpu, cpu->map[MAP_IO_PORTS].mem.data);
+	cpu->sfr->BIOS = 1; // disable BIOS for now
 
+    // Write some opcodes into ROM
 	ROM->data[0] = 0x00; // NOP
 	ROM->data[1] = 0xCB;
 	ROM->data[2] = 0x88; // clear bit 1 of reg B
@@ -92,15 +96,13 @@ int main(int argc, char *argv[]){
 	ROM->data[5] = 0xCB;
 	ROM->data[6] = 0x31; // swap reg C
 
-	Cpu *cpu = NULL;
-	cpu = cpu_Init(cpu_map[MAP_IO_PORTS].mem.data);
-
 	cpu->B = 0xFF;
 	cpu->C = 0xAB;
-	cpu_ExecuteOpcode(cpu, ROM->data); // NOP
-	cpu_ExecuteOpcode(cpu, ROM->data); // clear bit 1 of reg B
-	cpu_ExecuteOpcode(cpu, ROM->data); // clear bit 0 of reg B
-	cpu_ExecuteOpcode(cpu, ROM->data); // swap C
+
+	cpu_ExecuteOpcode(cpu); // NOP
+	cpu_ExecuteOpcode(cpu); // clear bit 1 of reg B
+	cpu_ExecuteOpcode(cpu); // clear bit 0 of reg B
+	cpu_ExecuteOpcode(cpu); // swap C
 
 	printf("cpu->B = #%02X\n", cpu->B); // result should be 0xFD
 	// Checking if double register have the correct endianness (reg B = MSB, reg C = LSB)
@@ -121,7 +123,10 @@ int main(int argc, char *argv[]){
 	// Checking if Special register and corresponding bits are correct
 	cpu->sfr->NR_50_bits.S01_volume = 0x4;
 	cpu->sfr->NR_50_bits.S02_volume = 0x1;
+	printf("cpu->sfr->NR_50_bits.S01_volume = #%02X\n", cpu->sfr->NR_50_bits.S01_volume);
+	printf("cpu->sfr->NR_50_bits.S02_volume = #%02X\n", cpu->sfr->NR_50_bits.S02_volume);
 
+    printf("Free CPU & exit\n");
 	cpu_Free(cpu);
 	cpu = NULL;
 	return 0;
