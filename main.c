4ef75d4207cdbe3c46403b9ad003b33da59a2868
pyroesp <nicolaslopezesp@gmail.com>
2018-03-29 03:17:55 +0200

    changed printf to the new debug_printf macro

--- main.c
+++ main.c
@@ -21,36 +21,36 @@ int main(int argc, char *argv[]){
 	vm->cpu->B = 0xFF;
 	vm->cpu->C = 0xAB;
 
-	printf("cpu->BC = #%04X\n", vm->cpu->BC); // result should be 0xFFAB
+	DEBUG_PRINTF("cpu->BC = #%04X\n", vm->cpu->BC); // result should be 0xFFAB
 
 	cpu_Run(vm->cpu); // NOP
 	cpu_Run(vm->cpu); // clear bit 1 of reg B
 	cpu_Run(vm->cpu); // clear bit 0 of reg B
 	cpu_Run(vm->cpu); // swap C
 
-	printf("cpu->B = #%02X\n", vm->cpu->B); // result should be 0xFD
+	DEBUG_PRINTF("cpu->B = #%02X\n", vm->cpu->B); // result should be 0xFD
 	// Checking if double register have the correct endianness (reg B = MSB, reg C = LSB)
-	printf("cpu->BC = #%04X\n", vm->cpu->BC); // result should be 0xBA
+	DEBUG_PRINTF("cpu->BC = #%04X\n", vm->cpu->BC); // result should be 0xBA
 	// Checking if reg array is correctly aligned with the registers
-	printf("cpu->reg[B] = #%02X\n", vm->cpu->reg[0]->R);
-	printf("cpu->reg[C] = #%02X\n", vm->cpu->reg[1]->R);
+	DEBUG_PRINTF("cpu->reg[B] = #%02X\n", vm->cpu->reg[0]->R);
+	DEBUG_PRINTF("cpu->reg[C] = #%02X\n", vm->cpu->reg[1]->R);
 	// Checking bits are correct
-	printf("cpu->reg[B].0 = #%02X\n", vm->cpu->reg[0]->R_bits.bit_0);
-	printf("cpu->reg[B].1 = #%02X\n", vm->cpu->reg[0]->R_bits.bit_1);
-	printf("cpu->reg[B].2 = #%02X\n", vm->cpu->reg[0]->R_bits.bit_2);
-	printf("cpu->reg[B].3 = #%02X\n", vm->cpu->reg[0]->R_bits.bit_3);
-	printf("cpu->reg[C].0 = #%02X\n", vm->cpu->reg[1]->R_bits.bit_0);
-	printf("cpu->reg[C].1 = #%02X\n", vm->cpu->reg[1]->R_bits.bit_1);
-	printf("cpu->reg[C].2 = #%02X\n", vm->cpu->reg[1]->R_bits.bit_2);
-	printf("cpu->reg[C].3 = #%02X\n", vm->cpu->reg[1]->R_bits.bit_3);
+	DEBUG_PRINTF("cpu->reg[B].0 = #%02X\n", vm->cpu->reg[0]->R_bits.bit_0);
+	DEBUG_PRINTF("cpu->reg[B].1 = #%02X\n", vm->cpu->reg[0]->R_bits.bit_1);
+	DEBUG_PRINTF("cpu->reg[B].2 = #%02X\n", vm->cpu->reg[0]->R_bits.bit_2);
+	DEBUG_PRINTF("cpu->reg[B].3 = #%02X\n", vm->cpu->reg[0]->R_bits.bit_3);
+	DEBUG_PRINTF("cpu->reg[C].0 = #%02X\n", vm->cpu->reg[1]->R_bits.bit_0);
+	DEBUG_PRINTF("cpu->reg[C].1 = #%02X\n", vm->cpu->reg[1]->R_bits.bit_1);
+	DEBUG_PRINTF("cpu->reg[C].2 = #%02X\n", vm->cpu->reg[1]->R_bits.bit_2);
+	DEBUG_PRINTF("cpu->reg[C].3 = #%02X\n", vm->cpu->reg[1]->R_bits.bit_3);
 
 	// Checking if Special register and corresponding bits are correct
 	vm->cpu->sfr->NR_50_bits.S01_volume = 0x4;
 	vm->cpu->sfr->NR_50_bits.S02_volume = 0x1;
-	printf("cpu->sfr->NR_50_bits.S01_volume = #%02X\n", vm->cpu->sfr->NR_50_bits.S01_volume);
-	printf("cpu->sfr->NR_50_bits.S02_volume = #%02X\n", vm->cpu->sfr->NR_50_bits.S02_volume);
+	DEBUG_PRINTF("cpu->sfr->NR_50_bits.S01_volume = #%02X\n", vm->cpu->sfr->NR_50_bits.S01_volume);
+	DEBUG_PRINTF("cpu->sfr->NR_50_bits.S02_volume = #%02X\n", vm->cpu->sfr->NR_50_bits.S02_volume);
 
-    printf("Free stuff & exit\n");
+    DEBUG_PRINTF("Free stuff & exit\n");
 	vm_Free(vm);
 	return 0;
 }
